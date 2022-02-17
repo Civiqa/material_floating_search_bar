7af5ee585036109c79637cd3673e75b65a35762b
Aly Ibrahim <bruinaly@gmail.com>
2022-02-17 10:21:03 -0500

    transiti0n duration

--- lib/src/floating_search_bar.dart
+++ lib/src/floating_search_bar.dart
@@ -395,7 +395,8 @@ class FloatingSearchBar extends ImplicitlyAnimatedWidget {
     this.scrollPadding = const EdgeInsets.symmetric(vertical: 16),
     this.showCursor = true,
     bool initiallyHidden = false,
-  })  : showAfter = showAfter ?? (initiallyHidden ? const Duration(days: 1) : null),
+  })  : showAfter =
+            showAfter ?? (initiallyHidden ? const Duration(days: 1) : null),
         super(key, implicitDuration, implicitCurve);
 
   @override
@@ -406,8 +407,8 @@ class FloatingSearchBar extends ImplicitlyAnimatedWidget {
   }
 }
 
-class FloatingSearchBarState
-    extends ImplicitlyAnimatedWidgetState<FloatingSearchBarStyle, FloatingSearchBar> {
+class FloatingSearchBarState extends ImplicitlyAnimatedWidgetState<
+    FloatingSearchBarStyle, FloatingSearchBar> {
   final GlobalKey<FloatingSearchAppBarState> barKey = GlobalKey();
   FloatingSearchAppBarState? get barState => barKey.currentState;
 
@@ -420,11 +421,12 @@ class FloatingSearchBarState
       }
     });
 
-  late CurvedAnimation animation = CurvedAnimation(parent: _controller, curve: curve);
+  late CurvedAnimation animation =
+      CurvedAnimation(parent: _controller, curve: curve);
 
   late final _translateController = AnimationController(
     vsync: this,
-    duration: const Duration(milliseconds: 800),
+    duration: const Duration(milliseconds: 200),
   );
 
   late var _translateAnimation = CurvedAnimation(
@@ -513,7 +515,8 @@ class FloatingSearchBarState
       transition.searchBar = this;
     }
 
-    if (widget.scrollController != null && widget.scrollController != _scrollController) {
+    if (widget.scrollController != null &&
+        widget.scrollController != _scrollController) {
       _scrollController = widget.scrollController!;
     }
 
@@ -583,7 +586,8 @@ class FloatingSearchBarState
           return false;
         }
 
-        _translateController.value += delta / (style.height + style.margins.top);
+        _translateController.value +=
+            delta / (style.height + style.margins.top);
         _lastPixel = pixel;
       }
     }
@@ -683,7 +687,8 @@ class FloatingSearchBarState
     return AnimatedAlign(
       duration: isAnimating ? duration : Duration.zero,
       curve: widget.transitionCurve,
-      alignment: Alignment(isOpen ? style.openAxisAlignment : style.axisAlignment, -1.0),
+      alignment: Alignment(
+          isOpen ? style.openAxisAlignment : style.axisAlignment, -1.0),
       child: transition.isBodyInsideSearchBar
           ? bar
           : Column(
@@ -720,7 +725,8 @@ class FloatingSearchBarState
       onQueryChanged: widget.onQueryChanged,
       onSubmitted: widget.onSubmitted,
       progress: widget.progress,
-      automaticallyImplyDrawerHamburger: widget.automaticallyImplyDrawerHamburger,
+      automaticallyImplyDrawerHamburger:
+          widget.automaticallyImplyDrawerHamburger,
       automaticallyImplyBackButton: widget.automaticallyImplyBackButton,
       toolbarOptions: widget.toolbarOptions,
       transitionDuration: widget.transitionDuration,
@@ -845,20 +851,23 @@ class FloatingSearchBarState
       maxWidth: widget.width,
       openMaxWidth: widget.openWidth,
       axisAlignment: widget.axisAlignment ?? 0.0,
-      openAxisAlignment: widget.openAxisAlignment ?? widget.axisAlignment ?? 0.0,
+      openAxisAlignment:
+          widget.openAxisAlignment ?? widget.axisAlignment ?? 0.0,
       backgroundColor: widget.backgroundColor ?? theme.cardColor,
       shadowColor: widget.shadowColor ?? Colors.black45,
-      backdropColor:
-          widget.backdropColor ?? widget.transition?.backdropColor ?? Colors.black26,
+      backdropColor: widget.backdropColor ??
+          widget.transition?.backdropColor ??
+          Colors.black26,
       border: widget.border ?? BorderSide.none,
       borderRadius: widget.borderRadius ?? BorderRadius.circular(4),
       margins: (widget.margins ??
-              EdgeInsets.fromLTRB(8, MediaQuery.of(context).viewPadding.top + 6, 8, 0))
+              EdgeInsets.fromLTRB(
+                  8, MediaQuery.of(context).viewPadding.top + 6, 8, 0))
           .resolve(direction),
       padding: widget.padding?.resolve(direction) ??
           const EdgeInsets.symmetric(horizontal: 12),
-      insets:
-          widget.insets?.resolve(direction) ?? const EdgeInsets.symmetric(horizontal: 8),
+      insets: widget.insets?.resolve(direction) ??
+          const EdgeInsets.symmetric(horizontal: 8),
     );
   }
 
